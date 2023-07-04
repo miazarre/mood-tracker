27900cf49542c61a423b7004de1c6d6c50fe483f
miazarre <mia.g.dev@gmail.com>
2023-07-04 16:32:03 +0200

    Adding Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
@@ -8,6 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <main className="min-h-screen m-2">
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp />} />
---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="flex flex-col items-center text-slate-950">
+      <div className="bg-design-mood h-40 w-full"></div>
+      <h1 className="relative sm:my-10 md:my-20 font-main sm:text-3xl md:text-9xl">
+        Let's keep this mood on track
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
